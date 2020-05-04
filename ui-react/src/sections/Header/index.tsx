9387d81e8bc3c7978c44d511ee12f85a122c757c
Eduards Egle <eduards@egle.me>
2020-05-04 20:12:43 +0300

    Sidebar

--- ui-react/src/sections/Header/index.tsx
+++ ui-react/src/sections/Header/index.tsx
@@ -1,16 +1,15 @@
-import React, { FC } from "react";
+import React from "react";
+import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import { ListItemIcon } from "@material-ui/core";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
 const useStyles = makeStyles({
   list: {
@@ -26,14 +25,14 @@ const Header = () => {
 
   const [drawer, toggleDrawer] = React.useState(false);
 
-  const list = (anchor: Anchor) => (
+  const list = () => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
+        // [classes.fullList]: anchor === "top" || anchor === "bottom",
       })}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onClick={() => toggleDrawer(false)}
+      onKeyDown={() => toggleDrawer(false)}
     >
       <List>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
@@ -61,24 +60,8 @@ const Header = () => {
 
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            onClick={() => toggleDrawer(true)}
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-
       <Drawer anchor={"left"} open={drawer} onClose={() => toggleDrawer(false)}>
-        <div>Rooms</div>
-        <div>Playlists</div>
+        {list()}
       </Drawer>
     </>
   );
